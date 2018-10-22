67a686754e0955c9e14fff3237d4e69d3bcf86d2
hungHoangDang <hungdh131@gmail.com>
2018-10-22 09:11:59 +0700

    new update

--- event-emitter/main.js
+++ event-emitter/main.js
@@ -1,44 +1,44 @@
 class EventEmitter {
     constructor() {
-        this.event = {}
+      this.events = {};
     }
-
-    subscribe( eventName, fn ) {
-        if (!this.events[eventName]) {
-            this.events[eventName] = [];
-        }
-
-        this.events[eventName].push(fn);
-
-        return () => {
-            this.event[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
-        }
-    }
-
+    
     emit(eventName, data) {
-        const event = this.events[eventName];
-        if(event) {
-            event.forEach( fn => {
-                fn.call(null, data);
-            })
-        }
+      const event = this.events[eventName];
+      if( event ) {
+        event.forEach(fn => {
+          fn.call(null, data);
+        });
+      }
     }
-}
-
-let input = document.querySelector('input[type="text"]');
-let button = document.querySelector('button');
-let h1 = document.querySelector('h1');
-
-button.addEventListener('click', () => {
+    
+    subscribe(eventName, fn) {
+      if(!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+      
+      this.events[eventName].push(fn);
+      return () => {
+        this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
+      }
+    }
+  }
+  
+    document.addEventListener("DOMContentLoaded", function(event) {
+      let input = document.querySelector('input[type="text"]');
+  let button = document.querySelector('button');
+  let h1 = document.querySelector('h1');
+  
+  button.addEventListener('click', () => {
     emitter.emit('event:name-changed', {name: input.value});
-});
-
-let emitter = new EventEmitter();
-
-emitter.subscribe('event:name-changed', data => {
+  });
+  
+  let emitter = new EventEmitter();
+  emitter.subscribe('event:name-changed', data => {
     h1.innerHTML = `Your name is: ${data.name}`;
-});
-
-let unsubscribe = emitter.subscribe('event:name-changed', data => console.log(data));
-
-unsubscribe();
+  });
+    });
+  
+  
+  
+  
